493d3a279adbbaf1bc3c6f85843f60228901df00
PavelKravets5 <y-gfdtk.98@mail.ru>
2019-11-01 20:27:11 +0300

    Update Program.cs

--- Module_4_Task_7/Module_4_Task_7/Program.cs
+++ Module_4_Task_7/Module_4_Task_7/Program.cs
@@ -83,7 +83,7 @@ namespace Module_4_Task_7
             while (!check)
             {
                 check = int.TryParse(Console.ReadLine(), out arrSize);
-                if (check == false || arrSize < 0 || arrSize == 0)
+                if (check == false || arrSize <= 0)
                 {
                     check = false;
                     Console.WriteLine("Некорректно, еще раз");
