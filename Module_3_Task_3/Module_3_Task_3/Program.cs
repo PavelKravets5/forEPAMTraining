d39e30af218a5bb73637be042f681c65bbaf452a
PavelKravets5 <y-gfdtk.98@mail.ru>
2019-11-20 12:25:13 +0300

    try_to_resolve_merge_conflict_2

---
+++ Module_3_Task_3/Module_3_Task_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Module_3_Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n;
+            bool check = int.TryParse(Console.ReadLine(), out n);
+            while (!check)
+            {
+                Console.WriteLine("Некорректно, еще раз");
+                check = int.TryParse(Console.ReadLine(), out n);
+            }
+
+
+            if (n >= 1)
+            {
+                Console.WriteLine("1-ое число Фибоначи: 0");
+            }
+            if(n>=2)
+            {
+                Console.WriteLine("2-ое число Фибоначи: 1");
+
+                uint first = 0, second = 1, temp=0, i = 2;
+                while (i < n)
+                {
+                    i++;
+                    temp = second;
+                    second = first + second;
+                    first = temp;
+                    Console.WriteLine($"{i}-ое число Фибоначи: {second}");
+                    
+                }
+
+            }
+            Console.WriteLine("Завершено");
+            
+        }
+    }
+}
---
+++ Module_3_Task_5/Module_3_Task_5/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Module_3_Task_5
+{
+    class Program
+    {
+        static int ReadWithCheckInt()
+        {
+            int num = 0;
+            bool check = int.TryParse(Console.ReadLine(), out num);
+            while (!check)
+            {
+                Console.WriteLine("Некорректно, еще раз");
+                check = int.TryParse(Console.ReadLine(), out num);
+            }
+            return num;
+        }
+
+        const int upperLimit = 9;
+        const int lowetLimit = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводите число:");
+            int num;
+            bool check = int.TryParse(Console.ReadLine(), out num);
+            while (!check)
+            {
+                Console.WriteLine("Некорректно, еще раз");
+                check = int.TryParse(Console.ReadLine(), out num);
+            }
+
+            Console.WriteLine("Вводите удаляемую одну цифру:");
+            int x = -1; check = false;
+            while (!check)
+            { 
+                check = int.TryParse(Console.ReadLine(), out x);
+                if(check==false || x< lowetLimit || x> upperLimit)
+                {
+                    check = false;
+                    Console.WriteLine("Некорректно, еще раз");
+                }
+            }
+
+            int reverseNum = 0 ;
+            bool sign = true;
+            if(num<0)
+            {
+                num = Math.Abs(num);
+                sign = false;
+            }
+
+            while(num>0)
+            {
+                int temp = num % 10;
+                num /= 10;
+                if (temp!=x)
+                {
+                    reverseNum += temp;
+                    reverseNum *= 10;
+                }
+            }
+
+            while(reverseNum > 0)
+            {
+                num += reverseNum % 10;
+                num *= 10;
+                reverseNum /= 10;
+            }
+            num /= 10;
+
+            if(sign==false)
+            {
+                num = - num;
+            }
+            Console.WriteLine($"Результат, удалены все вхождения заданной литеры, : {num} \nЗавершено");
+        }
+    }
+}
